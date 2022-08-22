0d30bdf64036dbec0e7ae2683011c168109391ac
Caleb <caleblpena@gmail.com>
2022-08-22 10:32:16 -0600

    formatted document

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
-
-const app = express()
+const { Classroom, JobListings } = require('./schemas.js');
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
+	res.render('home');
+});
 
 app.get('/class/create', (req, res) => {
-      res.render('create')
-})
+	res.render('create');
+});
+
+app.post('/class/create', (req, res) => {
+	res.render('create');
+});
 
 app.get('/class/:id', (req, res) => {
-  res.render('jobListings')
-})
+	res.render('jobListings');
+});
 
 app.get('/class/:id/dashboard', (req, res) => {
-  res.render('dashboard')
-})
+	res.render('dashboard');
+});
 
 app.get('/class/:id/admin', (req, res) => {
-  res.render('admin')
-})
+	res.render('admin');
+});
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
+	console.log('Listening on port 3000');
+});
--- schemas.js
+++ schemas.js
@@ -1,31 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/cp_job_listings");
+	await mongoose.connect('mongodb://localhost:27017/cp_job_listings');
 }
 const Schema = mongoose.Schema;
 
 const classroom = new Schema({
-    className: {type: String, required: true},
-    leaders: [{ type: mongoose.ObjectId, ref: "users"}],
-    admin: [{ type: mongoose.ObjectId, ref: "users" }],
-    owner: { type: mongoose.ObjectId, ref: "users", required: true },
-    jobListings: [{type: mongoose.ObjectId, ref: "jobListings"}]
-})
+	className: { type: String, required: true },
+	leaders: [{ type: mongoose.ObjectId, ref: 'users' }],
+	admin: [{ type: mongoose.ObjectId, ref: 'users' }],
+	owner: { type: mongoose.ObjectId, ref: 'users', required: true },
+	jobListings: [{ type: mongoose.ObjectId, ref: 'jobListings' }],
+});
 
 const Classroom = mongoose.model('classroom', classroom);
 
 const jobListings = new Schema({
-    jobTitle: {type: String, required: true},
-    company: {type: String, required: true},
-    careerTracks: [{type: String, required: true}],
-    salary: {type: Number, required: true},
-    salaryType: {type: String, required: true, enum: ['hourly', 'yearly']},
-    description: {type: String, required: true},
-    tags: [{type: String, required: true, enum: ['Core', 'Alumni', 'Temp', 'Part Time', 'Remote']}]
-})
+	jobTitle: { type: String, required: true },
+	company: { type: String, required: true },
+	careerTracks: [{ type: String, required: true }],
+	salary: { type: Number, required: true },
+	salaryType: { type: String, required: true, enum: ['hourly', 'yearly'] },
+	description: { type: String, required: true },
+	tags: [
+		{
+			type: String,
+			required: true,
+			enum: ['Core', 'Alumni', 'Temp', 'Part Time', 'Remote'],
+		},
+	],
+});
 
 const JobListings = mongoose.model('jobListings', jobListings);
 
