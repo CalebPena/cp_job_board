6cb2b1cb60adbe1238e40501ea7d7b3952572694
Caleb <caleblpena@gmail.com>
2022-08-22 09:31:13 -0600

    added basic routes

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/class/create', (req, res) => {
+      res.send('Hello World!')
+})
+
+app.get('/class/:id', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/class/:id/dashboard', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/class/:id/admin', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
