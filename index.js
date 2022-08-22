4af27670e0caa593fa82a32b2fd2bd48a94bcad6
Caleb <caleblpena@gmail.com>
2022-08-22 11:25:58 -0600

    added login and register routes

--- index.js
+++ index.js
@@ -45,6 +45,21 @@ app.get('/class/:id/admin', (req, res) => {
 	res.render('admin');
 });
 
+app.get('/login', (req, res) => {
+	res.render('login');
+});
+
+app.post('/login', (req, res) => {
+	res.redirect('/');
+});
+
+app.get('/register', (req, res) => {
+	res.render('register');
+});
+app.post('/register', (req, res) => {
+	res.redirect('/');
+});
+
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
 });
