937fe2577a19f48ba3a260f9f6b0fb66d1040986
Caleb <caleblpena@gmail.com>
2023-05-29 18:05:45 -0600

    add toggle visibility to password

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -14,3 +14,27 @@ const navbarLinks = document.getElementsByClassName('nav-links')[0];
 toggleButton.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active');
 });
+
+function insertAfter(referenceNode, newNode) {
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+const passwordInput = document.querySelector('[type=password]');
+const togglePasswordContainer = document.createElement('div');
+togglePasswordContainer.classList.add('show-password');
+const togglePasswordLabel = document.createElement('label');
+togglePasswordLabel.setAttribute('for', 'toggle-password');
+togglePasswordLabel.innerText = 'Show password';
+const togglePassword = document.createElement('input');
+togglePassword.id = 'toggle-password';
+togglePassword.setAttribute('type', 'checkbox');
+togglePassword.addEventListener('click', () => {
+	if (passwordInput.type === 'password') {
+		passwordInput.setAttribute('type', 'text');
+	} else {
+		passwordInput.setAttribute('type', 'password');
+	}
+});
+togglePasswordContainer.appendChild(togglePasswordLabel);
+togglePasswordContainer.appendChild(togglePassword);
+insertAfter(passwordInput, togglePasswordContainer);
